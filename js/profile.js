c7e3033e8732b48b06e7017c4111bf4438262ccb
Joey <joeychung960@gmail.com>
2016-09-15 23:01:37 -0700

    Added js logic in for my picture on index. Reclassified a lot of the HTML and CSS properties for index.html. Adjusted the CSS for mobile on that page. Fixed a typo on portfolio.

--- js/profile.js
+++ js/profile.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     $("#profile").mouseenter(function(){
-        $("#profile-about-me").fadeIn(1000);
-        $("#profile").css("opacity",.3);
+        $("#profile-about-me").fadeTo(500,1);
+        $("#profile-about-me").css("z-index",5);
+        $("#profile").fadeTo(600,.3);
     });
     $("#profile-about-me").mouseleave(function(){
-        $("#profile-about-me").fadeOut(1000);
-        $("#profile").css("opacity",1);
+        $("#profile-about-me").fadeTo(500,.3);
+        $("#profile-about-me").css("z-index",-5);
+        $("#profile").fadeTo(600,1);
     });
 });
